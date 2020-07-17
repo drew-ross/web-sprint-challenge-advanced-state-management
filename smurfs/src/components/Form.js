a791b83e2fe9172ab6343fa046d4420fd737076a
Drew Ross <mail.drew.ross@gmail.com>
2020-07-17 09:29:21 -0700

    Create form inputs, map to component state, use handleChanges. No submit action.

--- smurfs/src/components/Form.js
+++ smurfs/src/components/Form.js
@@ -1,10 +1,61 @@
 import React, { useState, useEffect } from 'react';
 
+const initialFormValues = {
+    name: '',
+    age: 1,
+    height: 5
+};
+
 const Form = props => {
 
+    const [formValues, setFormValues] = useState(initialFormValues);
+
+    const handleChanges = e => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        });
+    };
+
     return (
-        <p>Yes</p>
-    )
+        <form className='Form'>
+            <label htmlFor='formName'>Name</label>
+            <input
+                name='name'
+                id='formName'
+                type='text'
+                value={formValues.name}
+                onChange={handleChanges}
+                required
+            />
+            <br />
+            <label htmlFor='formAge'>Age</label>
+            <input
+                name='age'
+                id='formAge'
+                type='number'
+                min='0'
+                value={formValues.age}
+                onChange={handleChanges}
+                required
+            />
+            <br />
+            <label htmlFor='formHeight'>Height</label>
+            <input
+                name='height'
+                id='formHeight'
+                type='number'
+                min='1'
+                value={formValues.height}
+                onChange={handleChanges}
+                required
+            />
+            <span>cm</span>
+            <br />
+            <button>Register</button>
+
+        </form>
+    );
 };
 
 export default Form;
